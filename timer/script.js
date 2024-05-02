7a23cf0475f6ececa3fd5fdfcb505f7695f9d613
michael.abatangelo <michael.abaangelo@enfapitreviglio.com>
2024-05-02 12:29:51 +0200

    add git

---
+++ timer/script.js
@@ -0,0 +1,44 @@
+//bottone start & reset
+var intervalId;
+var running = false;
+
+
+function startStopTimer() {
+    var display = document.querySelector('#timer');
+
+    if (!running) { 
+        var duration = 60;
+        var timer = duration, minutes, seconds;
+
+        intervalId = setInterval(function () {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.textContent = minutes + ":" + seconds;
+
+            if (--timer < 0) {
+                clearInterval(intervalId);
+                running = false;
+                document.getElementById('toggleButton').textContent = "Start Timer";
+            }
+        }, 1000);
+
+        running = true;
+        document.getElementById('toggleButton').textContent = "Stop Timer";
+    } else {
+        clearInterval(intervalId);
+        running = false;
+        document.getElementById('toggleButton').textContent = "Start Timer";
+    }
+}
+
+
+//bottone reset
+function resetStopTimer() {
+    clearInterval(intervalId); 
+    var display = document.querySelector('#timer');
+    display.textContent = "01:00";
+}
