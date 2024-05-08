c1295ba31e632bafc28b38fcc57bb054167c5a44
Michael2610 <167307892+Michael2610@users.noreply.github.com>
2024-05-08 11:24:54 +0200

    tolta funzione linea

--- progettotimer/script.js
+++ progettotimer/script.js
@@ -42,20 +42,3 @@ function resetStopTimer() {
     var display = document.querySelector('#timer');
     display.textContent = "01:00";
 }
-
-//linea che diminuisce
-
-function countdown() {
-    let seconds = 60;
-  
-    const interval = setInterval(() => {
-      console.log(`${seconds}s`);
-      seconds--;
-  
-    if (seconds < 0) {
-        clearInterval(interval);
-    }
-  }, 1000);
-}
-  
-countdown();
